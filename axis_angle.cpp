12bfe18dcb40ac4236fd5fce3997bc79abf92220
Rafael Gomez <rgome020@wch132-07.cs.ucr.edu>
2018-02-28 15:54:03 -0800

    lab8

---
+++ axis_angle.cpp
@@ -0,0 +1,16 @@
+#include "axis_angle.h"
+void axis_angle::from_angle_and_axis(float angle,const vec3& axis)
+{
+    //TODO:compute the axis-angle parameter (w) from the input rotation angle (in degrees) and axis
+    // w =
+    w = angle * axis; 
+}
+void axis_angle::to_angle_and_axis(float& angle,vec3& axis) const
+{
+    //TODO: compute the angle (in degrees) and the axis of the rotation, from the axis-angle parameter w
+    // angle = 
+    // axis = 
+    axis = w.normalized();
+    angle = w.magnitude();
+}
+
