f3f45bf53691463379efb067d2973d3311615da8
Rafael Gomez <rgome020@ucr.edu>
2018-03-01 14:10:17 -0800

    Update axis_angle.cpp

--- axis_angle.cpp
+++ axis_angle.cpp
@@ -3,14 +3,15 @@ void axis_angle::from_angle_and_axis(float angle,const vec3& axis)
 {
     //TODO:compute the axis-angle parameter (w) from the input rotation angle (in degrees) and axis
     // w =
-    w = angle * axis; 
+    w = (angle * M_PI*180) * axis.normalized(); 
 }
 void axis_angle::to_angle_and_axis(float& angle,vec3& axis) const
 {
     //TODO: compute the angle (in degrees) and the axis of the rotation, from the axis-angle parameter w
     // angle = 
     // axis = 
-    axis = w.normalized();
-    angle = w.magnitude();
+    
+    axis = w.magnitude() * (180/M_PI);
+    angle = w.normalized();
 }
 
